15eb1548a5983316de1932ae5859c25ef27f45fa
kooroot <koo.minjae@jinnhyun.com>
2023-03-20 16:22:17 +0900

    update

--- src/Dex.sol
+++ src/Dex.sol
@@ -1,18 +1,40 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Dex {
-    uint256 public number;
+import "./interfaces/IDex.sol";
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
 
-    constructor(address tokenX, address tokenY) {
-        
+contract Dex is IDex, ERC20 {
+    address public tokenX;
+    address public tokenY;
+
+
+
+    constructor(address _tokenX, address _tokenY) ERC20("DreamLP", "DLP") {
+        tokenX = ERC20(_tokenX);
+        tokenY = ERC20(_tokenY);
+        lpToken = ERC20(address.this);
     }
 
-    function setNumber(uint256 newNumber) public {
+    function swap(ERC20 sourceToken, ERC20 targetToken) public returns (bool) {
+        require(address(sourceToken) != address(targetToken));
+
+        uint256 approve = targetToken.allowance(msg.sender, this);
+        uint256 balance = sourceToken.balanceOf(address(this));
+
+        require(balance >= approve);
+
+        require(targetToken.transferFrom(msg.sender, this, approve));
+        require(sourceToken.tranfer(msg.sender, approve));
+
+        return true;
+    }
+
+    function addLiquidity(uint256 tokenXAmount, uint256 tokenYAmount, uint256 minimumLPToeknAmount) public return (uin256) {
         number = newNumber;
     }
 
-    function increment() public {
+    function removeLiquidity(uint256 LPTokenAmount, uint256 minimumTokenXAmount, uint256 minimumTokenYAmount) public return (uint256, uint256){
         number++;
     }
 }
