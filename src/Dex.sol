56953163c7a14ee3cfa444c46a916134128994da
koor00t <minjae.koo@dream-academy.io>
2023-03-21 03:41:08 +0900

    test addLiquidity

--- src/Dex.sol
+++ src/Dex.sol
@@ -5,9 +5,13 @@ import "./interfaces/IDex.sol";
 import "openzeppelin-contracts/token/ERC20/ERC20.sol";
 
 contract Dex is IDex, ERC20 {
+    uint public constant MINIMUM_LIQUIDITY = 10**3;
     ERC20 public tokenX;
     ERC20 public tokenY;
     ERC20 public lpToken;
+
+    uint private reserveX;
+    uint private reserveY;
 
     constructor(address _tokenX, address _tokenY) ERC20("DreamLP", "DLP") {
         tokenX = ERC20(_tokenX);
@@ -15,22 +19,34 @@ contract Dex is IDex, ERC20 {
         lpToken = ERC20(address(this));
     }
 
-    function addLiquidity(uint256 tokenXAmount, uint256 tokenYAmount, uint256 minimumLPToeknAmount) public returns (uint256) {
+    function addLiquidity(uint256 tokenXAmount, uint256 tokenYAmount, uint256 minimumLPToeknAmount) public returns (uint256 LPTokenAmount) {
         require(tokenXAmount > 0 && tokenYAmount > 0, "Token must be not zero.");
-        uint256 tokenXreserved;
-        uint256 tokenYreserved;
-        uint256 tokenXliquidity;
-        uint256 tokenYliquidity;
-        uint256 lptokenmatch;
+        reserveX = tokenX.balanceOf(address(this));
+        reserveY = tokenY.balanceOf(address(this));
+        uint256 liquidity;
 
-        
+        uint256 _totalSupply = totalSupply();
+        if (_totalSupply == 0) {
+            LPTokenAmount = sqrt((tokenXAmount + reserveX) * (tokenYAmount + reserveY) / MINIMUM_LIQUIDITY);
+        } else {
+            require(reserveX * tokenYAmount == reserveY * tokenXAmount, "Add Liquidity Error");
+            LPTokenAmount = min(_totalSupply * tokenXAmount / reserveX, _totalSupply * tokenYAmount / reserveY);
+        }
+        require(LPTokenAmount >= minimumLPToeknAmount, "Minimum LP Error");
+        _mint(msg.sender, LPTokenAmount);
+        reserveX += tokenXAmount;
+        reserveY += tokenYAmount;
+        tokenX.transferFrom(msg.sender, address(this), tokenXAmount);
+        tokenY.transferFrom(msg.sender, address(this), tokenYAmount);
 
-
+        emit AddLiquidity(msg.sender, tokenXAmount, tokenYAmount);
+        return LPTokenAmount;
 
     }
 
     function removeLiquidity(uint256 LPTokenAmount, uint256 minimumTokenXAmount, uint256 minimumTokenYAmount) public override returns (uint256, uint256){
         require(LPTokenAmount > 0, "Token must be not zero.");
+        uint _totalSupply = totalSupply();
 
         _burn(msg.sender, LPTokenAmount);
         
@@ -38,6 +54,7 @@ contract Dex is IDex, ERC20 {
 
     function swap(uint256 tokenXAmount, uint256 tokenYAmount, uint256 tokenMinimumOutputAmount) public override returns (uint256) {
         require(tokenXAmount > 0 && tokenYAmount > 0, "Token must be not zero.");
+
     }
 
     function transfer(address to, uint256 lpAmount) public override(ERC20, IDex) returns (bool) {
@@ -57,6 +74,22 @@ contract Dex is IDex, ERC20 {
             z = 1;
         }
         // else z = 0 (default value)
+    }
+
+    function min(uint x, uint y) internal pure returns (uint z) {
+        z = x < y ? x : y;
+    }
+
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z=x+y) >= x, "ds-math-add-overflow");
+    }
+
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z=x-y) <= x, 'ds-math-sub-underflow');
+    }
+
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z=x*y) / y == x, 'ds-math-mul-overflow');
     }
 
 }
