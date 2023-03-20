41a92f94e01eb2333014d07c0f7a5b45b14f27cd
koor00t <minjae.koo@dream-academy.io>
2023-03-20 20:00:40 +0900

    modified

--- src/Dex.sol
+++ src/Dex.sol
@@ -7,8 +7,7 @@ import "openzeppelin-contracts/token/ERC20/ERC20.sol";
 contract Dex is IDex, ERC20 {
     address public tokenX;
     address public tokenY;
-
-
+    address public lpToken;
 
     constructor(address _tokenX, address _tokenY) ERC20("DreamLP", "DLP") {
         tokenX = ERC20(_tokenX);
@@ -19,22 +18,22 @@ contract Dex is IDex, ERC20 {
     function swap(ERC20 sourceToken, ERC20 targetToken) public returns (bool) {
         require(address(sourceToken) != address(targetToken));
 
-        uint256 approve = targetToken.allowance(msg.sender, this);
+        uint256 approved = targetToken.allowance(msg.sender, this);
         uint256 balance = sourceToken.balanceOf(address(this));
 
-        require(balance >= approve);
+        require(balance >= approved);
 
-        require(targetToken.transferFrom(msg.sender, this, approve));
-        require(sourceToken.tranfer(msg.sender, approve));
+        require(targetToken.transferFrom(msg.sender, this, approved));
+        require(sourceToken.tranfer(msg.sender, approved));
 
         return true;
     }
 
-    function addLiquidity(uint256 tokenXAmount, uint256 tokenYAmount, uint256 minimumLPToeknAmount) public return (uin256) {
-        number = newNumber;
+    function addLiquidity(uint256 tokenXAmount, uint256 tokenYAmount, uint256 minimumLPToeknAmount) public returns (uint256) {
+        
     }
 
-    function removeLiquidity(uint256 LPTokenAmount, uint256 minimumTokenXAmount, uint256 minimumTokenYAmount) public return (uint256, uint256){
-        number++;
+    function removeLiquidity(uint256 LPTokenAmount, uint256 minimumTokenXAmount, uint256 minimumTokenYAmount) public returns (uint256, uint256){
+        
     }
 }
