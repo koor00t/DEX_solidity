7dbda73d7d0928fb2bff9c00e35ca6146875cc0d
koor00t <minjae.koo@dream-academy.io>
2023-03-20 20:37:35 +0900

    Test run completed

--- src/Dex.sol
+++ src/Dex.sol
@@ -5,35 +5,29 @@ import "./interfaces/IDex.sol";
 import "openzeppelin-contracts/token/ERC20/ERC20.sol";
 
 contract Dex is IDex, ERC20 {
-    address public tokenX;
-    address public tokenY;
-    address public lpToken;
+    ERC20 public tokenX;
+    ERC20 public tokenY;
+    ERC20 public lpToken;
 
     constructor(address _tokenX, address _tokenY) ERC20("DreamLP", "DLP") {
         tokenX = ERC20(_tokenX);
         tokenY = ERC20(_tokenY);
-        lpToken = ERC20(address.this);
-    }
-
-    function swap(ERC20 sourceToken, ERC20 targetToken) public returns (bool) {
-        require(address(sourceToken) != address(targetToken));
-
-        uint256 approved = targetToken.allowance(msg.sender, this);
-        uint256 balance = sourceToken.balanceOf(address(this));
-
-        require(balance >= approved);
-
-        require(targetToken.transferFrom(msg.sender, this, approved));
-        require(sourceToken.tranfer(msg.sender, approved));
-
-        return true;
+        lpToken = ERC20(address(this));
     }
 
     function addLiquidity(uint256 tokenXAmount, uint256 tokenYAmount, uint256 minimumLPToeknAmount) public returns (uint256) {
+
+    }
+
+    function removeLiquidity(uint256 LPTokenAmount, uint256 minimumTokenXAmount, uint256 minimumTokenYAmount) public override returns (uint256, uint256){
         
     }
 
-    function removeLiquidity(uint256 LPTokenAmount, uint256 minimumTokenXAmount, uint256 minimumTokenYAmount) public returns (uint256, uint256){
-        
+    function swap(uint256 tokenXAmount, uint256 tokenYAmount, uint256 tokenMinimumOutputAmount) public override returns (uint256) {
     }
+
+    function transfer(address to, uint256 lpAmount) public override(ERC20, IDex) returns (bool) {
+
+    }
+
 }
--- src/interfaces/IDex.sol
+++ src/interfaces/IDex.sol
@@ -12,7 +12,7 @@ function transfer(address to, uint256 lpAmount) external returns (bool);
 interface IDex {
     function swap(uint256 tokenXAmount, uint256 tokenYAmount, uint256 tokenMinimumOutputAmount) external returns (uint256 outputAmount);
     function addLiquidity(uint256 tokenXAmount, uint256 tokenYAmount, uint256 minimumLPTokenAmount) external returns (uint256 LPTokenAmount);
-    function removeLiquidity(uint256 LPTokenAmount, uint256 minimumTokenXAmount, uint256 minimumTokenYAmount) external;
+    function removeLiquidity(uint256 LPTokenAmount, uint256 minimumTokenXAmount, uint256 minimumTokenYAmount) external returns (uint256 tokenXAmount, uint256 tokenYAmount);
     function transfer(address to, uint256 lpAmount) external returns (bool);
     
     event Swap();
